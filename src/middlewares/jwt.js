327683d36141a301e217de360e7db10edeafaa4d
keys8410 <yanalmeidagarcia@gmail.com>
2020-10-26 16:42:54 -0300

    middleware jwt/auth com corre

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,22 +1,27 @@
 const { verifyJwt, getTokenFromHeaders } = require('../helpers/jwt');
 const SECTOR_CONFIG = require('../config/sector');
+const { getMessages } = require('../helpers/messages');
 
 const checkAuthMaster = (req, res, next) => {};
 
 const checkAuthCoord = (req, res, next) => {
   const token = getTokenFromHeaders(req.headers);
-  if (!token) return res.jsonUnauthorized(null, 'Invalid token - token');
+  if (!token)
+    return res.jsonUnauthorized(null, getMessages('auth.invalid.token'));
 
   try {
     const decoded = verifyJwt(token);
 
     if (SECTOR_CONFIG.coord !== decoded.sector)
-      return res.jsonUnauthorized(null, 'Invalid token - sector');
+      return res.jsonUnauthorized(null, getMessages('auth.invalid.sector'));
 
     next();
   } catch (error) {
     console.log(error);
-    return res.jsonUnauthorized(null, 'Invalid token - jwt expired');
+    return res.jsonUnauthorized(
+      null,
+      getMessages('auth.invalid.expired_token'),
+    );
   }
 };
 
