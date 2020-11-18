a7646a1f6f686d7ea8a5de1996341a410cd06004
Keys8410 <yanalmeidagarcia@gmail.com>
2020-11-18 03:35:45 -0300

    [UPDATE] adicionando rotas e controllers: sectors/genders

--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ const cors = require('cors');
 const morgan = require('morgan');
 
 const response = require('./middlewares/response');
+
 const controllerUser = require('./controllers/users');
 const controllerAuth = require('./controllers/auth');
+const controllerGenders = require('./controllers/genders');
+const controllerSectors = require('./controllers/sectors');
 
 app.use(response);
 app.use(morgan('dev'));
@@ -15,7 +18,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-app.use('/v1/users', controllerUser);
 app.use('/v1/auth', controllerAuth);
+app.use('/v1/users', controllerUser);
+app.use('/v1/genders', controllerGenders);
+app.use('/v1/sectors', controllerSectors);
 
 module.exports = app;
